f1d58f0055fae80e320d9775551d3ab2111478a8
ghaniz <ghanizahid@hotmail.com>
2023-08-29 23:09:32 -0400

    Week 1 class code

---
+++ Week 1/Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println();
+        System.out.println("This Program Produces");
+        //System.out.println("four lines of output");
+        /* System.out.println("/ \\ // \\\\ /// \\\\\\");
+        System.out.println("This program prints a \n quote from the Gettysburg Address. \n\n \"Four score and seven years ago, \n our 'fore fathers' brought forth on \n this continent a new nation.\"");
+*/
+    }
+}
