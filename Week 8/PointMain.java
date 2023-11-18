6c2fefee26306b764319dd2af26ca41c1b3987d1
ghaniz <ghanizahid@hotmail.com>
2023-11-18 13:25:23 -0500

    Week 8 code files

---
+++ Week 8/PointMain.java
@@ -0,0 +1,20 @@
+public class PointMain {
+    public class Point {
+        int x;
+        int y;
+    }
+    public static void main(String[] args) {
+        // create two Point objects
+        Point p1 = new Point();
+        p1.y = 2;
+        Point p2 = new Point();
+        p2.x = 4;
+
+        System.out.println(p1.x + ", " + p1.y);   // 0, 2
+
+        // move p2 and then print it
+        p2.x += 2;
+        p2.y++;
+        System.out.println(p2.x + ", " + p2.y);   // 6, 1
+    }
+}
